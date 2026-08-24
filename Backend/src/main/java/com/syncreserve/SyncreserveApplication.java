cc91b6570a9e192f7f20dcaa00f0abff83dfb5ab
Saranga <sarangamadhubhashani2001@gmail.com>
2026-08-24 23:07:49 +0530

    chore: initialize SyncReserve project

---
+++ Backend/src/main/java/com/syncreserve/SyncreserveApplication.java
@@ -0,0 +1,13 @@
+package com.syncreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncreserveApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/syncreserve/SyncreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
