3853095180ceb3d014a0092323aa8b8079d46cae
Saranga <sarangamadhubhashani2001@gmail.com>
2026-08-26 22:57:48 +0530

    feat: implement event details and seat reservation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Events from "./pages/Events";
+import EventDetails from "./pages/EventDetails";
 
 function App() {
   return (
@@ -26,7 +27,10 @@ function App() {
         element={<Events />}
       />
 
-
+      <Route
+        path="/events/:eventId"
+        element={<EventDetails />}
+      />
 
 
     </Routes>
